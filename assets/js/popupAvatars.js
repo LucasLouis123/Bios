df1476e77e4aa3d3c4c5663825951a6847426a60
Deer0009 <124927303+LucasLouis123@users.noreply.github.com>
2024-09-25 14:33:03 +0200

    Update popupAvatars.js

--- assets/js/popupAvatars.js
+++ assets/js/popupAvatars.js
@@ -1,5 +1,6 @@
 async function fetchAvatarsForAll() {
     const liElements = document.querySelectorAll('#popup li');
+    const corsProxy = 'https://cors-anywhere.herokuapp.com/';
 
     for (let li of liElements) {
         const imgElement = li.querySelector('img');
@@ -7,19 +8,26 @@ async function fetchAvatarsForAll() {
         if (imgElement) {
             const userId = imgElement.alt;
 
+            imgElement.src = "assets/img/black.png";
+
             if (userId) {
                 try {
-                    const response = await fetch(`https://185.228.81.59:3000/api/avatar/${userId}`);
+                    let response = await fetch(`https://api.wxrn.lol/api/avatar/${userId}`);
+                    
+                    if (!response.ok) {
+                        response = await fetch(`${corsProxy}https://185.228.81.59:3000/api/avatar/${userId}`);
+                    }
+
                     const data = await response.json();
 
                     if (data.avatarUrl) {
-                        // Update the image src with the fetched avatar URL
                         imgElement.src = data.avatarUrl;
                     } else if (data.error) {
                         console.error(`Error for user ${userId}: ${data.error}`);
                     }
                 } catch (error) {
-                    console.error(`Error fetching avatar for user ${userId}:`, error);
+                    console.error(`Failed to fetch avatar for user ${userId}:`, error);
+                    continue;
                 }
             } else {
                 console.error('No Discord User ID found in the alt attribute.');
