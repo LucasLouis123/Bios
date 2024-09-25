d23428f632ad7e91be6a1c737ad070d403678a5f
Deer0009 <124927303+LucasLouis123@users.noreply.github.com>
2024-09-25 14:47:53 +0200

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -11,6 +11,7 @@ const tracks = [
     { title: "Destroy Lonely - if looks could kill", path: "assets/music/iflookscouldkill.mp3" },
     { title: "Ken Carson - Succubus", path: "assets/music/Succubus.mp3" },
     { title: "Chii Wvttz - Shoot Pt.2", path: "assets/music/music.mp3" },
+    { title: "UglyEnd - Heyfever", path: "assets/music/Heyfever.mp3" },
     { title: "Don Toliver - Bandit", path: "assets/music/Bandit.mp3" },
     { title: "Yeat - Shade", path: "assets/music/Shade.mp3" },
     { title: "che x SEMATARY - 666", path: "assets/music/666.mp3" },
